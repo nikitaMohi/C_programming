5feac4a4a5ab878d73b2bb073a92fedfce00960b
nikitamohi <mohitenikita26@gmail.com>
2026-09-26 18:23:11 +0530

    Add the pointer in c

---
+++ PointerDemo.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char ch ='A';
+    int no=11;
+    float marks=98.56f;
+    double d=90.56789;
+    printf("%d\n",sizeof(ch));
+    printf("%d\n",sizeof(no));
+    printf("%d\n",sizeof(marks));
+    printf("%d\n",sizeof(d));
+
+    return 0;
+
+}
---
+++ PointerDemo4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    char ch ='A';
+    int no=11;
+    float marks=98.56f;
+    double d=90.56789;
+    
+    char *cp=&ch;
+    int *ip=&no;
+    float *fp=&marks;
+    double *dp=&d;
+    
+    printf("%c\n",*cp);
+    printf("%d\n",*ip);
+    printf("%f\n",*fp);
+    printf("%lf\n",*dp);
+    
+    return 0;
+
+}
+}
